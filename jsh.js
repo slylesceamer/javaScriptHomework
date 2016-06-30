becd6a4b1a9c9520ea27946572093f17dfb18e2d
Kyle Creamer <kmc427@scarletmail.rutgers.edu>
2016-06-30 18:21:27 -0400

    update

---
+++ jsh.js
@@ -0,0 +1,25 @@
+alert("Welcome");
+alert("to");
+alert("my");
+alert("site");
+
+
+
+function logToHTML() {
+	document.getElementById("change").innerHTML = "After";
+};
+
+console.log("Hello World")
+
+var x = 2000;
+var y = 16;
+alert(x + y); 
+
+var colors = ["red", "blue", "orange", "yellow", "indigo", "green", "violet"]
+	console.log(colors [0]) 
+	console.log(colors [2]) 
+	console.log(colors [3])
+	console.log(colors [5])
+	console.log(colors [1])
+	console.log(colors [4])
+	console.log(colors [6])
